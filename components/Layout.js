5bed46dcab659fee672fe7a3199639d9cf0cbff7
Irabaruta Freeman <50771819+snow-blade@users.noreply.github.com>
2020-07-10 18:42:59 +0200

    fixed some layout bugs

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
     <h1 className="mb-8">
       <Link href="/">
         <a className="text-6xl font-black text-black no-underline">
-          Next.Js Starter Blog
+          Unblog
         </a>
       </Link>
     </h1>
@@ -17,7 +17,7 @@ export default function Layout({ children }) {
     <h1 className="mb-2">
       <Link href="/">
         <a className="text-2xl font-black text-black no-underline">
-          Next.Js Starter Blog
+         Unblog
         </a>
       </Link>
     </h1>
