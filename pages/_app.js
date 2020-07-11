7ef8ad58841dcdf9736577fe378f03d629e3c531
Irabaruta Freeman <50771819+snow-blade@users.noreply.github.com>
2020-07-11 12:26:36 +0200

    added the progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,12 @@ import "styles/tailwind.css";
 import "styles/index.css"
 import "typeface-open-sans";
 import "typeface-merriweather";
-
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
