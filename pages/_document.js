a7ad188988400ec59b0e7ed120de5371d64e54ff
Irabaruta Freeman <50771819+snow-blade@users.noreply.github.com>
2020-07-14 16:55:49 +0200

    fixed bugs

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,21 @@ export default class MyDocument extends Document {
     return (
       <Html lang={siteMetadata.language}>
         <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-X47H18WLG2"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-X47H18WLG2');
+            </script>
+              `,
+          }}
+        />
           <TypographyStyle typography={typography} />
         </Head>
         <body>
